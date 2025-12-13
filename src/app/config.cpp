1b078016efe094bd09ac46572c93c70055ad8465
Yavik Kapadia <yavikkapadia@gmail.com>
2025-12-12 23:53:45 -0800

    chore: Bump version to 1.5.9

--- src/app/config.cpp
+++ src/app/config.cpp
@@ -13,7 +13,7 @@ namespace html2ndi {
 
 namespace {
 
-const char* VERSION = "1.5.8";
+const char* VERSION = "1.5.9";
 
 void print_arg(const char* short_opt, const char* long_opt, 
                const char* arg, const char* desc) {
